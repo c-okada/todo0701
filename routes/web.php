5a6294356a17164529ba80d08111721b5c873034
c-okada <hello.tra.okada@gmail.com>
2022-07-05 09:45:40 +0900

    フォルダ作成完成版

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::get('/folders/{id}/tasks','TaskController@index')->name('tasks.index');
 //フォルダ作成機能
 Route::get('/folders/create','FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create','FolderController@create');
+
+//タスク作成機能
